a5fb3c745d2a8fad07c079fced8e6c3751b650d1
Muhammad Junaid <junnedbutt@gmail.com>
2025-02-28 00:31:16 -0800

    header fix

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,11 +3,11 @@ import { FaBars } from 'react-icons/fa';
 
 function Navbar() {
   return (
-    <header className='p-5 text-white'>
-    <nav className='text-white   flex items-center justify-between gap-10'>
-      <a className='font-bold text-lg md:text-3xl lg:text-3xl' href=''>JuniVenture</a>
+    <header className='pl-2 pt-5 text-white'>
+    <nav className='text-white flex items-center justify-between gap-10'>
+      <a className='font-bold text-lg pl-2 md:text-3xl lg:text-3xl' href=''>JuniVenture</a>
 
-      <div className='flex items-center font-bold text-lg gap-5 pr-3   lg:gap-14 md:text-xl lg:text-2xl '>
+      <div className='flex items-center font-bold gap-5 pr-9   lg:gap-14 md:text-xl lg:text-2xl '>
       <h1 className='whitespace-nowrap cursor-pointer'>This Week's Deals</h1>
       <button className='bg-transparent border-2 border-transparent cursor-pointer'>LogIn</button>
       </div>
